991ef9104f14fba89e686f02296e28152b4a672a
perivi8 <perivihari8@gmail.com>
2025-08-14 16:39:48 +0530

    Describe your new changes

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://employee-management-system-1-3evk.onrender.com'
+  apiUrl: 'https://employee-management-system-1-3evk.onrender.com/api'
 };
